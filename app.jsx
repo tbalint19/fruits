f5c10095759a68ee013ece79a9ee1212ea71dcd8
tbalint19 <tothbalint719@gmail.com>
2021-11-10 12:31:14 +0100

    function props

--- app.jsx
+++ app.jsx
@@ -1,9 +1,14 @@
-const Fruit = ({ fruit }) => {
+const Fruit = ({ fruit, onSave }) => {
 
-    const [ val, setVal ] = React.useState("")
+    const [ val, setVal ] = React.useState(fruit.name)
     const [ shouldShowInput, setShouldShowInput ] = React.useState(false)
 
     const toggle = () => setShouldShowInput(!shouldShowInput)
+
+    const save = () => {
+        onSave(fruit.id, val)
+        setShouldShowInput(false)
+    }
 
     return (
         <div className="card">
@@ -18,6 +23,9 @@ const Fruit = ({ fruit }) => {
                 </h4>
             }
             <button onClick={toggle}>{ shouldShowInput ? "Cancel" : "Edit" }</button>
+
+            { shouldShowInput && <button onClick={save}>Save</button> }
+            { shouldShowInput && <button onClick={() => setVal(fruit.name)}>Reset</button> }
         </div>
     )
 }
@@ -27,23 +35,27 @@ const App = () => {
     const [ val, setVal ] = React.useState("")
 
     const [ fruits, setFruits ] = React.useState([
-        { name: "alma", color: "red" },
-        { name: "körte", color: "yellow" },
-        { name: "szilva", color: "purple" }
+        { id: getId(), name: "alma", color: "red" },
+        { id: getId(), name: "körte", color: "yellow" },
+        { id: getId(), name: "szilva", color: "purple" }
     ])
 
     const clickHandler = () => {
-        setFruits([ ...fruits, { name: val, color: "unknown" } ])
+        setFruits([ ...fruits, { id: getId(), name: val, color: "unknown" } ])
         setVal("")
+    }
+
+    const updateFruit = (id, newName) => {
+        setFruits(fruits.map(fruit => fruit.id === id ? { ...fruit, name: newName } : fruit))
     }
 
     return (
         <div id="app-container">
             <input type="text" value={val} onChange={(event) => setVal(event.target.value)}/>
 
-            <button onClick={clickHandler}>Click me</button>
+            <button onClick={clickHandler}>Add</button>
 
-            { fruits.map(fruit => <Fruit fruit={fruit}/> ) }
+            { fruits.map(fruit => <Fruit key={fruit.id} fruit={fruit} onSave={updateFruit}/> ) }
         </div>
     )
 }
